1168a1b7a1f3eadc21fc9af6d5ab789aba141561
Henrique Tavares <ihenrits@gmail.com>
2025-09-15 08:01:40 -0300

    feat: implement ReduxProvider and integrate AddTodo and TodoList components in App

--- rocketseat/react-redux-zustand/src/App.tsx
+++ rocketseat/react-redux-zustand/src/App.tsx
@@ -1,5 +1,16 @@
+import { Provider as ReduxProvider } from 'react-redux';
+
+import { store } from "./store";
+
+import { AddTodo } from "./components/AddTodo";
+import { TodoList } from "./components/TodoList";
 
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <ReduxProvider store={store}>
+      <TodoList />
+      <AddTodo />
+    </ReduxProvider>
+  )
 }
 
