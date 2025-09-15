d3c3fc821a70dbb116e3ec2ffe616f2bb810e779
Henrique Tavares <ihenrits@gmail.com>
2025-09-15 08:14:33 -0300

    refactor: replace useSelector with useAppSelector in TodoList component

--- rocketseat/react-redux-zustand/src/components/TodoList.tsx
+++ rocketseat/react-redux-zustand/src/components/TodoList.tsx
@@ -1,11 +1,9 @@
-import { useSelector } from 'react-redux';
+import { useAppSelector } from "../store";
 
 export function TodoList() {
-  const todos = useSelector((store) => {
-    return store.todo
+  const todos = useAppSelector((store) => {
+    return store.todo;
   });
-
-  console.log(todos);
 
   return (
     <ul>
