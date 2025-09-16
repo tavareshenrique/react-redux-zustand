a5e8b3989894649f7af6bffcf31805d3928ae9c1
Henrique Tavares <ihenrits@gmail.com>
2025-09-16 07:41:54 -0300

    feat: refactor application structure and integrate Tailwind CSS
    - Added PostCSS configuration for Tailwind CSS and Autoprefixer.
    - Removed AddTodo and TodoList components to streamline the application.
    - Introduced Player component for a new feature.
    - Updated global styles to include Tailwind CSS directives.
    - Configured Tailwind CSS with a new configuration file.
    - Cleaned up Redux store by removing todo slice and related logic.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,27 +1,10 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore,  } from '@reduxjs/toolkit';
 
 import { useSelector } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
 
-const todoSlice = createSlice({
-  name: 'todo',
-  initialState: ['Fazer Café', 'Estudar Redux', 'Ler um Livro'] as string[],
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo);
-    },
-    remove: (state, action) => {
-      return state.filter((_, index) => index !== action.payload);
-    },
-  },
-});
-
-export const { add, remove } = todoSlice.actions;
-
 export const store = configureStore({
-  reducer: {
-    todo: todoSlice.reducer,
-  },
+  reducer: {},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
