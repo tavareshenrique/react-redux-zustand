c6ca3a3fb39dc39b13285098ed5b0ef7995a2e28
Henrique Tavares <ihenrits@gmail.com>
2025-09-17 08:14:26 -0300

    feat: add onEnded handler to Video component for automatic lesson progression

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,13 @@
+import { useDispatch } from "react-redux";
+
 import ReactPlayer from "react-player";
+
 import { useAppSelector } from "../store";
+import { next } from "../store/slices/player";
 
 export function Video() {
+  const dispatch = useDispatch()
+
   const lesson = useAppSelector((state) => {
     const { currentModuleIndex, currentLessonIndex } = state.player;
 
@@ -10,12 +16,17 @@ export function Video() {
     return currentLesson;
   })
 
+  function handlePlayNext() {
+    dispatch(next())
+  }
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
+        onEnded={handlePlayNext}
         src={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
