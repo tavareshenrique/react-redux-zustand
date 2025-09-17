5a30e72ceb8c81e458df0ee5b9d0a27790ba0d48
Henrique Tavares <ihenrits@gmail.com>
2025-09-17 07:41:59 -0300

    feat: implement Redux store for player state management and refactor Module and Player components to utilize lessons and modules from the store

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,8 +3,11 @@ import { MessageCircle } from "lucide-react";
 import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
+import { useAppSelector } from "../store";
 
 export function Player() {
+  const modules = useAppSelector((state) => state.player.course.modules);
+
   return (
     <div className="flex items-center justify-center h-screen bg-zinc-950 text-zinc-50">
       <div className="flex w-[1100px] flex-col gap-6">
@@ -23,9 +26,14 @@ export function Player() {
           </div>
 
           <aside className="absolute top-0 bottom-0 right-0 overflow-y-scroll border-l divide-y-2 borde-l divide-zinc-900 w-80 border-zinc-800 bg-zinc-900 scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800 ">
-            <Module moduleIndex={0} title="Desvendando o Redux" amountOfLessons={3} />
-            <Module moduleIndex={1} title="Desvendando o Redux" amountOfLessons={3} />
-            <Module moduleIndex={2} title="Desvendando o Redux" amountOfLessons={3} />
+            {modules.map((module, index) => (
+              <Module
+                key={module.id}
+                moduleIndex={index}
+                title={module.title}
+                amountOfLessons={module.lessons.length}
+              />
+            ))}
           </aside>
         </main>
       </div>
