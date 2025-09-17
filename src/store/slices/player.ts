47b6318c37d8fb3509d7b018e3e1d8c361e0c4c3
Henrique Tavares <ihenrits@gmail.com>
2025-09-17 07:55:00 -0300

    feat: add initial state and play reducer to player slice for lesson navigation

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -57,8 +57,17 @@ const playerSlice = createSlice({
         },
       ],
     },
+    currentLessonIndex: 0,
+    currentModuleIndex: 0,
   },
-  reducers: {},
+  reducers: {
+    play: (state, action) => {
+      state.currentModuleIndex = action.payload[0];
+      state.currentLessonIndex = action.payload[1];
+    }
+  },
 });
 
 export const player = playerSlice.reducer;
+
+export const { play } = playerSlice.actions;
