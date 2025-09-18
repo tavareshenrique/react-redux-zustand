458a58fb33e3db6227b3c12e9e18cafa2a9f0065
Henrique Tavares <ihenrits@gmail.com>
2025-09-18 07:49:03 -0300

    feat: export playerSlice for external usage

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,7 +1,7 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import { useAppSelector } from "..";
 
-const playerSlice = createSlice({
+export const playerSlice = createSlice({
   name: "player",
   initialState: {
     course: {
