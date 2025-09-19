6059554ea8e3813e5a888a8ed02b885e3051fa18
Henrique Tavares <ihenrits@gmail.com>
2025-09-19 08:04:16 -0300

    feat: enhance Video component with loading state and refactor code structure

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,30 +1,34 @@
-
 import ReactPlayer from "react-player";
 
 import { next, useCurrentLesson } from "../store/slices/player";
-import { useAppDispatch } from "../store";
+import { useAppDispatch, useAppSelector } from "../store";
+import { Loader } from "lucide-react";
 
 export function Video() {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
+
   const { currentLesson } = useCurrentLesson();
+  const isCourseLoading = useAppSelector((state) => state.player.isLoading);
 
   function handlePlayNext() {
-    dispatch(next())
-  }
-
-  if (!currentLesson) {
-    return null;
+    dispatch(next());
   }
 
   return (
     <div className="w-full bg-zinc-950 aspect-video">
-      <ReactPlayer
-        width="100%"
-        height="100%"
-        controls
-        onEnded={handlePlayNext}
-        src={`https://www.youtube.com/watch?v=${currentLesson.id}`}
-      />
+      {isCourseLoading ? (
+        <div className="flex items-center justify-center h-full">
+          <Loader className="w-6 h-6 text-zinc-400 animate-spin" />
+        </div>
+      ) : (
+        <ReactPlayer
+          width="100%"
+          height="100%"
+          controls
+          onEnded={handlePlayNext}
+          src={`https://www.youtube.com/watch?v=${currentLesson?.id}`}
+        />
+      )}
     </div>
   );
 }
