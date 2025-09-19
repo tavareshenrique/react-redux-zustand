2625bf82466e2da88e9d02d8a69920ed0aa5384a
Henrique Tavares <ihenrits@gmail.com>
2025-09-19 07:58:20 -0300

    feat: add useAppDispatch hook for typed dispatch in store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import { configureStore,  } from '@reduxjs/toolkit';
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
 import { player } from "./slices/player";
 
@@ -11,5 +11,7 @@ export const store = configureStore({
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch: () => AppDispatch = useDispatch;
